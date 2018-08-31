e5fb724256837ede777fa3cb32015c86f0b50666
toywei <g1e2t3@qq.com>
2018-08-31 18:53:16 +0800

    go解析html，对比字符串处理和goquery类库;反爬虫应对http://cn.sonhoo.com/wukong/a192372；

--- collyApp.go
+++ collyApp.go
@@ -27,6 +27,7 @@ http://www.cnhan.com/pinfo/
 http://www.heze.cn/info
 http://www.heze.cn/qiye/
 
+http://cn.sonhoo.com/wukong/
 采集站点当日更新数据的客户联系方式
 
 */
@@ -73,13 +74,19 @@ func eleInArr(ele string, arr [] string) bool {
 	return false
 }
 
+// Structured representation for github file name table
+type example struct {
+	Title string   `goquery:"h1"`
+	Files []string `goquery:"table.files tbody tr.js-navigation-item td.content,text"`
+}
+
 func getTodayUrls() []string {
 	var todayUrls []string
 	PotentialCustomerWebSiteUrlSet := getTodaySpideredUrl()
 
 	// Instantiate default collector
 	c := colly.NewCollector(
-		colly.AllowedDomains("www.cnhan.com"),
+		colly.AllowedDomains("Twww.cnhan.com"),
 	)
 	// On every a element which has href attribute call callback
 	// 类选择器
@@ -96,10 +103,10 @@ func getTodayUrls() []string {
 	// Start scraping on http://www.cnhan.com/shantui/
 	c.Visit("http://www.cnhan.com/shantui/")
 
-	//起始路由改变
+	// 起始路由改变
 	// Instantiate default collector
 	c = colly.NewCollector(
-		colly.AllowedDomains("www.cnhan.com"),
+		colly.AllowedDomains("Twww.cnhan.com"),
 		colly.URLFilters(
 			//请求页面的正则表达式，满足其一即可
 			//http://www.cnhan.com/hyzx/
@@ -137,10 +144,10 @@ func getTodayUrls() []string {
 	// Start scraping on http://www.cnhan.com/shantui/
 	c.Visit("http://www.cnhan.com/hyzx/")
 
-	//起始路由改变
+	// 起始路由改变
 	// Instantiate default collector
 	c = colly.NewCollector(
-		colly.AllowedDomains("www.cnhan.com"),
+		colly.AllowedDomains("Twww.cnhan.com"),
 		colly.URLFilters(
 			//请求页面的正则表达式，满足其一即可
 			//http://www.cnhan.com/pinfo/
@@ -178,10 +185,10 @@ func getTodayUrls() []string {
 	// Start scraping on http://www.cnhan.com/shantui/
 	c.Visit("http://www.cnhan.com/pinfo/")
 
-	//起始路由改变
+	// 起始路由改变
 	// Instantiate default collector
 	c = colly.NewCollector(
-		colly.AllowedDomains("www.heze.cn"),
+		colly.AllowedDomains("Twww.heze.cn"),
 	)
 	// On every a element which has href attribute call callback
 	// 类选择器
@@ -189,11 +196,6 @@ func getTodayUrls() []string {
 		link := e.Attr("href")
 		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
 		todayUrls = append(todayUrls, link)
-	})
-
-	// Before making a request print "Visiting ..."
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
 	})
 
 	// Start scraping on http://www.cnhan.com/shantui/
@@ -211,12 +213,12 @@ func getTodayUrls() []string {
 	   缺点：爬爬取速度慢
 	*/
 
-	//起始路由改变
+	// 起始路由改变
 	//http://www.heze.cn/qiye/  该页面、其主体子页面，刷新，内容变化
 	//http://www.heze.cn/qiye/list-8.html
 	// Instantiate default collector
 	c = colly.NewCollector(
-		colly.AllowedDomains("www.heze.cn"),
+		colly.AllowedDomains("Twww.heze.cn"),
 		colly.URLFilters(
 			//请求页面的正则表达式，满足其一即可
 			regexp.MustCompile("^http://www.heze.cn/qiye/(.{0}$)|(list-\\d+-\\d+\\.html$)"),
@@ -249,11 +251,114 @@ func getTodayUrls() []string {
 	// Start scraping on http://www.heze.cn/qiye/
 	c.Visit("http://www.heze.cn/qiye/")
 
+	// 起始路由改变
+	// 当天页面部分存在于起始页且与飞当天页面混杂，无法通过选择器判断，需解析文本
+	// 全站过滤
+	// 类目页 http://cn.sonhoo.com/wukong/c133
+	// 文章页 http://cn.sonhoo.com/wukong/a191114
+	c = colly.NewCollector(
+		colly.AllowedDomains("cn.sonhoo.com"),
+		colly.URLFilters(
+			//请求页面的正则表达式，满足其一即可
+			regexp.MustCompile("^http://cn.sonhoo.com/wukong/$"),
+			//regexp.MustCompile("^http://cn.sonhoo.com/wukong/[ac]{1}\\d+$"),
+			regexp.MustCompile("^http://cn.sonhoo.com/wukong/[a]{1}\\d+$"),
+		),
+		// 不加UA，无数据
+		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"),
+
+
+	)
+	// Add callbacks to a Collector
+
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		// 对方网站内容不明，试探性强制退出遍历
+		if len(todayUrls) > 30 {
+			return
+		}
+		link := e.Attr("href")
+		//fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		c.Visit(e.Request.AbsoluteURL(link))
+		// 无法通过href的DOM相关节点判断时间
+	})
+
+	c.OnHTML(".app-col", func(e *colly.HTMLElement) {
+		fmt.Println("First column of a table row:", e.Text)
+		dat1 := e.ChildText(".app-news-detail")
+		fmt.Println(dat1)
+		dat := e.ChildText(".app-news-detail>.app-news-detail__meta>.app-news-detail__meta-item")
+		fmt.Println("c.OnHTML--->html")
+		fmt.Println(dat)
+	})
+
+	// On every a element which has href attribute call callback
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Finished", r.Request.URL)
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Something went wrong:", err)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Visited", r.Request.URL)
+		reqUrl := fmt.Sprint(r.Request.URL)
+
+		if strings.Contains(reqUrl, "http://cn.sonhoo.com/wukong/a") {
+			wholePageHtml := string(r.Body)
+			fmt.Println(wholePageHtml)
+			s := strings.Replace(wholePageHtml, " ", "", -1)
+			pageDate := strings.Split(s, "app-news-detail__meta-item\">")[1]
+			pageDate = strings.Split(pageDate, "li")[0]
+			datetime := time.Now().Format("2006-01-02")
+			if (strings.Contains(pageDate, datetime) ) {
+				comName := strings.Split(s, "app-owner-card__title\">")[1]
+				comName = strings.Split(comName, "</h3>")[0]
+				telPhone := strings.Split(s, "app-owner-card__btn-phone\">")[1]
+				telPhone = strings.Split(telPhone, "</a>")[0]
+				comService := strings.Split(s, "app-owner-card__service\">")[1]
+				comService = strings.Split(comService, "</p>")[0]
+				// 建立func时，如何确定参数的类型？
+				t := eleInArr(reqUrl, PotentialCustomerWebSiteUrlSet)
+				if (!t) {
+					strings.Replace(reqUrl, "\n", "", -1)
+					client, err := mongo.Connect(context.Background(), "mongodb://hbaseU:123@192.168.3.103:27017/hbase", nil)
+					db := client.Database("hbase")
+					coll := db.Collection("todayUrls")
+					if err != nil {
+						fmt.Println(err)
+					}
+					// 判断日期的同样方法，获取其他字段
+					result, err := coll.InsertOne(
+						context.Background(),
+						bson.NewDocument(
+							bson.EC.String("spiderDate", time.Now().Format("20060102")),
+							bson.EC.String("url", reqUrl),
+							bson.EC.String("html", wholePageHtml),
+							bson.EC.String("comName", comName),
+							bson.EC.String("telPhone", telPhone),
+							bson.EC.String("comService", comService),
+						))
+					fmt.Println(err)
+					fmt.Println(result)
+				}
+			}
+		}
+	})
+
+	// Start scraping on http://cn.sonhoo.com/wukong/
+	c.Visit("http://cn.sonhoo.com/wukong/")
+
 	return todayUrls
 }
 
 func main() {
-
 	var todayUrls = getTodayUrls()
 	fmt.Println(todayUrls)
 	fmt.Println(len(todayUrls))
@@ -268,7 +373,7 @@ func main() {
 			//client, err := mongo.Connect(context.Background(), "mongodb://192.168.3.103:27017?username=hbaseU&password=123", nil)
 			client, err := mongo.Connect(context.Background(), "mongodb://hbaseU:123@192.168.3.103:27017/hbase", nil)
 			db := client.Database("hbase")
-			coll := db.Collection("todayUrls")
+			coll := db.Collection("todayUrlsTEST")
 			if err != nil {
 				fmt.Println(err)
 			}
